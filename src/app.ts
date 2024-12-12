b657677aa63577da7e34a2947942f4b1bcea99bb
Source-Code-007 <utsho926@gmail.com>
2024-12-12 20:13:18 +0600

    updated cron job for prevent sleeping render deployed

--- src/app.ts
+++ src/app.ts
@@ -8,30 +8,30 @@ import {
 } from './app/middleware/errHandler'
 import router from './app/routes'
 import cookieParser from 'cookie-parser'
-// import axios from 'axios'
-// import { CronJob } from 'cron'
+import axios from 'axios'
+import { CronJob } from 'cron'
 
 const app = express()
 
 // // Create an axios instance with a longer timeout
-// const axiosInstance = axios.create({
-//   timeout: 30000, // 30 seconds timeout
-// })
+const axiosInstance = axios.create({
+  timeout: 30000, // 30 seconds timeout
+})
 
-// // Set up a cron job to ping the server every 10 minutes
-// const job = new CronJob('*/10 * * * *', () => {
-//   axiosInstance
-//     .get('https://utshofolio-server.onrender.com/api/v1')
-//     .then((response) => {
-//       console.log('😀🎉 Self-ping successful:', response.status)
-//     })
-//     .catch((error) => {
-//       console.error('😡 Self-ping failed:', error.message)
-//     })
-// })
+// Set up a cron job to ping the server every 10 minutes
+const job = new CronJob('*/10 * * * *', () => {
+  axiosInstance
+    .get('https://japalearn-server.onrender.com/api/v1')
+    .then((response) => {
+      console.log('😀🎉 Self-ping successful:', response.status)
+    })
+    .catch((error) => {
+      console.error('😡 Self-ping failed:', error.message)
+    })
+})
 
 // // Start the cron job
-// job.start()
+job.start()
 
 app.get('/api/v1', async (req, res) => {
   res.send('Japalearn home route!')
